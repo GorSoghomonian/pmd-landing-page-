a1429bb415c4bed3eb2a4b6639cc331e2c303c27
GorSoghomonian <drouzzer9@gmail.com>
2025-08-04 18:05:55 +0400

    industrial page

--- src/app/page.js
+++ src/app/page.js
@@ -10,30 +10,8 @@ const pacifico = Pacifico({
 export default function Home() {
   return (
     <main className="min-h-screen bg-white">
-      <header className="border-b border-black/10">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6">
-          <nav className="flex items-center justify-between px-8 py-4">
-            <div className={`text-2xl font-bold text-black ${pacifico.className}`} aria-label="Logo">Logo</div>
-            <ul className="flex items-center gap-8 fz-10 font-medium text-gray-700 ">
-              <li>
-                <Link href="/" className="font-medium transition-all duration-300 hover:text-blue-600 cursor-pointer text-blue-600">Home</Link>
-              </li>
-              <li>
-                <Link href="/about" className="hover:text-blue-600 transition">About</Link>
-              </li>
-              <li>
-                <Link href="/industries" className="hover:text-blue-600 transition">Industries</Link>
-              </li>
-              <li>
-                <Link href="/services" className="hover:text-blue-600 transition">Services ▾</Link>
-              </li>
-            </ul>
-          </nav>
-        </div>
-      </header>
-
       <section className="flex flex-col items-center justify-center px-4 py-24 text-center">
-        <h1 className="text-5xl md:text-6xl font-bold text-gray-900 mb-6">
+        <h1 className="text-5xl md:text-6xl font-bold text-gray-900 mb-6 mt-18">
           Welcome to <span className="text-blue-600">PMD Solutions</span>
         </h1>
         <p className="text-xl md:text-2xl text-gray-600 mb-8 ">
